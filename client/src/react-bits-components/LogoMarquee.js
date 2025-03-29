0b9db40526ec320f4f860456c78df4c596233117
Vishnu Sreekumaran Nair <200557136@student.georgianc.on.ca>
2025-03-28 22:41:52 -0400

    add duplicate skills for wider screens and add vignettes

--- client/src/react-bits-components/LogoMarquee.js
+++ client/src/react-bits-components/LogoMarquee.js
@@ -1,28 +1,36 @@
 import Marquee from 'react-fast-marquee';
 
 const LogoMarquee = ({ skills }) => {
+
+  const repeatedSkills = [...skills, ...skills, ...skills];
   return (
-    <Marquee
-      speed={40}
-      gradient={false}
-      pauseOnHover
-      className="py-6"
-    >
-      {skills.map((item, idx) => (
-        <div
-          key={idx}
-          className="flex items-center gap-2 bg-white/5 border border-white/10 rounded-full px-4 py-2 mx-3 text-sm text-gray-200 whitespace-nowrap shadow"
+    <div className="relative w-full py-6 overflow-hidden">
+
+      <div className="absolute left-0 top-0 h-full w-12 bg-gradient-to-r from-black to-transparent z-10 pointer-events-none" />
+      
+      <div className="absolute right-0 top-0 h-full w-12 bg-gradient-to-l from-black to-transparent z-10 pointer-events-none" />
+        <Marquee
+          speed={40}
+          gradient={false}
+          pauseOnHover
+          className="py-6"
         >
-          <img
-            src={item.icon}
-            alt={item.label}
-            className="w-6 h-6 object-contain"
-            loading="lazy"
-          />
-          <span>{item.label}</span>
-        </div>
-      ))}
-    </Marquee>
+          {repeatedSkills.map((item, idx) => (
+            <div
+              key={idx}
+              className="flex items-center gap-2 bg-white/5 border border-white/10 rounded-full px-4 py-2 mx-3 text-sm text-gray-200 whitespace-nowrap shadow"
+            >
+              <img
+                src={item.icon}
+                alt={item.label}
+                className="w-6 h-6 object-contain"
+                loading="lazy"
+              />
+              <span>{item.label}</span>
+            </div>
+          ))}
+        </Marquee>
+      </div>
   );
 };
 
